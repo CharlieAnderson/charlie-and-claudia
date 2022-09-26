c6ae99bc3e5836182d66303d4f7bed90b19da426
Charles Anderson <charlieanderson13@gmail.com>
2022-09-25 22:59:59 -0400

    new landing page

--- src/App.js
+++ src/App.js
@@ -4,16 +4,12 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <navbar className="navbar" />
       <div className="background-rotated"></div>
       <header className="App-header">
       </header>
         <div className="logo-container">
           <img className="App-logo"></img>
-          {/* <div className="App-logo">            
-            <div className="App-logo-border"></div>
-          </div> */}
-          <text className="App-logo-text" fontFamily='Heebo'>CHARLIE & CLAUDIA</text>
+          <div className="App-logo-text" fontFamily='Heebo'>COMING SOON</div>
         </div>
     </div>
   );
